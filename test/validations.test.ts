51dc11119088a683f0d277568b76ad881337571d
Diorgenes Morais <diorgenesmorais@gmail.com>
2023-09-05 20:52:49 -0300

    test: create the test first

---
+++ test/validations.test.ts
@@ -0,0 +1,8 @@
+import { Validations } from '../src/validations';
+
+describe('Validations', () => {
+    it('should create Validations class', () => {
+
+        expect(Validations).toBeTruthy();
+    })
+})
