a4197bfbfabf2fcc8aa08584586cf5ecaa453502
Diorgenes Morais <diorgenesmorais@gmail.com>
2023-09-05 21:09:04 -0300

    test: create formt-to test

---
+++ test/formtTo.test.ts
@@ -0,0 +1,7 @@
+import * as FormatTo from '../src/format-to'
+
+describe('FormatTo', () => {
+    it('should create function', () => {
+        expect(FormatTo.removeNonDigits).toBeTruthy();
+    })
+})
